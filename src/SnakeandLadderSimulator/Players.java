36177bd5310aadd9976ce2e36e694fe6ee16aff8
AkshatDahikar <akshatdahikar@gmail.com>
2024-11-27 16:06:35 +0530

    Added: Rolls Die

--- src/SnakeandLadderSimulator/Players.java
+++ src/SnakeandLadderSimulator/Players.java
@@ -1,10 +1,15 @@
 package SnakeandLadderSimulator;
 
+import java.util.Random;
+
 public class Players {
 	private static final int START_POSITION = 0;
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake And Ladder Game\n");
 		int playerPosition = START_POSITION;
 		System.out.println("Player starts at position:  " + playerPosition);
+		 Random random=new Random();
+		 int dieRoll=random.nextInt(6)+1;
+		 System.out.println("Player rolls the die and gets: "+dieRoll);
 	}
 }
