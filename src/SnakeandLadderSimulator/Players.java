60e2cc70e00b9c542671363148a6362137e2e38c
AkshatDahikar <akshatdahikar@gmail.com>
2024-11-27 15:25:55 +0530

    Added : Players Roll Die to get Number

--- src/SnakeandLadderSimulator/Players.java
+++ src/SnakeandLadderSimulator/Players.java
@@ -22,7 +22,7 @@ public class Players {
 	            	 playerPosition -= dieRoll;
 	            	 System.out.println("You cannot move beyond position  " + WINNING_POSITION);
 	            }else {
-	            	 System.out.println("Player is at position " + playerPosition);
+	            	 System.out.println("git statPlayer is at position " + playerPosition);
 	            	 playerPosition = checkSnakeAndLadder(playerPosition);
 
 	            }
@@ -38,6 +38,12 @@ public class Players {
 				position += 10;
 				System.out.println("Yay! You found a ladder! Moving up to " + position);
                 break;
+			}
+		}
+		for(int snake: SNAKES) {
+			if(position == snake) {
+				position -=10;
+				System.out.println("Oops ! You landed on a snake ! Moving down to"+position);
 			}
 		}
 		return position;
