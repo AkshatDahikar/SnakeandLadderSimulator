7ad7772f7a0589c5fba7ab79ec507da54fbe2d65
AkshatDahikar <akshatdahikar@gmail.com>
2024-11-27 16:01:03 +0530

    Added : Snake And Ladder Game Played

--- src/SnakeandLadderSimulator/Players.java
+++ src/SnakeandLadderSimulator/Players.java
@@ -5,6 +5,6 @@ public class Players {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake And Ladder Game\n");
 		int playerPosition = START_POSITION;
-		System.out.println("Player starts at position: " + playerPosition);
+		System.out.println("Player starts at position:  " + playerPosition);
 	}
 }
